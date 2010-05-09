19bf2f0003d3d01df692a7a546501132e2d8e240
Luca Grulla <luca.grulla@gmail.com>
2010-05-09 21:16:44 +0100

    Luca: refactored bookstore.js, now shopping cart contains all the related behaviours

--- src/scripts/bookstore.js
+++ src/scripts/bookstore.js
@@ -4,29 +4,11 @@ function ShoppingCart() {
 
     this.add = function(item) {
         items.push(item);
+        modifyView(item);
     };
 
-    this.orderPrice = function() {
-        var price = 0;
-        $.each(items, function(index, value) {
-            price = price + value.price;
-        });
-        return price;
-    }
-}
 
-$(document).ready(function() {
-    new ProductsPresenter(new ProductsView(), new ProductsService()).init();
-    
-    var cart = new ShoppingCart();
-    
-    $(".buy").live("submit", function(event) {
-
-        var item = {description: this["description"].value,
-                    price: parseInt(this["price"].value) };
-
-        cart.add(item);
-
+    function modifyView(item) {
         $(".itemList ul").append("<li>");
         $(".itemList ul li:last")
                 .append(item.description)
@@ -34,8 +16,29 @@ $(document).ready(function() {
                 .append(item.price);
         $(".itemList ul").append("</li>");
 
-        $(".totalPrice").text("Total price: " + cart.orderPrice());
-        
+        $(".totalPrice").text("Total price: " + orderPrice());
+    }
+
+    orderPrice = function() {
+        var price = 0;
+        $.each(items, function(index, value) {
+            price = price + value.price;
+        });
+        return price;
+    };
+}
+
+$(document).ready(function() {
+    new ProductsPresenter(new ProductsView(), new ProductsService()).init();
+
+    var cart = new ShoppingCart();
+    $(".buy").live("submit", function(event) {
+
+        var item = {description: this["description"].value,
+            price: parseInt(this["price"].value) };
+
+        cart.add(item);
+
         event.preventDefault();
     });
 });
