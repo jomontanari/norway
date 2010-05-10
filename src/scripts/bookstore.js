bfab4c6b9cf4a69a2255a0b2fd68f401e2b40df2
Luca Grulla <luca.grulla@gmail.com>
2010-05-10 08:38:11 +0100

    Luca:extracted shopping cart into a separate file

--- src/scripts/bookstore.js
+++ src/scripts/bookstore.js
@@ -1,33 +1,3 @@
-function ShoppingCart() {
-
-    var items = new Array();
-
-    this.add = function(item) {
-        items.push(item);
-        modifyView(item);
-    };
-
-
-    function modifyView(item) {
-        $(".itemList ul").append("<li>");
-        $(".itemList ul li:last")
-                .append(item.description)
-                .append(" - ")
-                .append(item.price);
-        $(".itemList ul").append("</li>");
-
-        $(".totalPrice").text("Total price: " + orderPrice());
-    }
-
-    orderPrice = function() {
-        var price = 0;
-        $.each(items, function(index, value) {
-            price = price + value.price;
-        });
-        return price;
-    };
-}
-
 $(document).ready(function() {
     new ProductsPresenter(new ProductsView(), new ProductsService()).init();
 
---
+++ src/scripts/shoppingCart.js
@@ -0,0 +1,29 @@
+function ShoppingCart() {
+
+    var items = new Array();
+
+    this.add = function(item) {
+        items.push(item);
+        modifyView(item);
+    };
+
+
+    function modifyView(item) {
+        $(".itemList ul").append("<li>");
+        $(".itemList ul li:last")
+                .append(item.description)
+                .append(" - ")
+                .append(item.price);
+        $(".itemList ul").append("</li>");
+
+        $(".totalPrice").text("Total price: " + orderPrice());
+    }
+
+    orderPrice = function() {
+        var price = 0;
+        $.each(items, function(index, value) {
+            price = price + value.price;
+        });
+        return price;
+    };
+}
