054b44974fed37ea3ff596d80661bc55d83d5e1d
Luca Grulla <luca.grulla@gmail.com>
2010-05-17 09:03:03 +0100

    Luca: refactored productsSorter to use jQuery.each for looping t[Ahe arrays

--- src/scripts/viewHelpers/tableBuilder.js
+++ src/scripts/viewHelpers/tableBuilder.js
@@ -1,45 +1,41 @@
 function TableBuilder(tableHeaders) {
     var headers = [];
-    var rows = [];
 
-
-    this.buildTable = function(rows) {
+    this.buildTable = function(items) {
         var returnString = "<table>";
 
         if (headers.length > 0) {
             returnString += "<thead><tr>";
 
-            for (var i = 0; i < headers.length; i++) {
-                returnString += "<th>" + headers[i] + "</th>";
-            }
+            $.each(headers, function(i, header) {
+                returnString += "<th>" + header + "</th>";
+            });
 
             returnString += "</tr></thead>";
         }
 
 
-        if (rows.length > 0) {
+        if (items.length > 0) {
             returnString += "<tbody>";
 
-            for (var i = 0; i < rows.length; i++) {
-                var rowData = rows[i];
-
+            $.each(items, function(i , anItem) {
                 returnString += '<tr class="' + (i % 2 == 0 ? 'even' : 'odd') + '">';
-                returnString += '<td class="image"><img src="' + rowData.image + '" alt="' + rowData.name + '"/></td>';
-                returnString += '<td class="name"><h5>' + rowData.name + '</h5></td>';
-                returnString += '<td class="price">&pound;' + rowData.price + '</td>';
+                returnString += '<td class="image"><img src="' + anItem.image + '" alt="' + anItem.name + '"/></td>';
+                returnString += '<td class="name"><h5>' + anItem.name + '</h5></td>';
+                returnString += '<td class="price">&pound;' + anItem.price + '</td>';
                 returnString += '<td class="buy">' +
-                                '   <form action="/item/add/' + rowData.id + '" method="POST" class="buy">' +
-                                        '<input type="hidden" name="description" value="' + rowData.name + '"/>' +
-                                        '<input type="hidden" name="price" value="' + rowData.price + '"/>' +
-                                        '<input type="submit" value="Add to cart">' +
-                                    '</form>' +
-                                '</td>';
+                        '   <form action="/item/add/' + anItem.id + '" method="POST" class="buy">' +
+                        '<input type="hidden" name="description" value="' + anItem.name + '"/>' +
+                        '<input type="hidden" name="price" value="' + anItem.price + '"/>' +
+                        '<input type="submit" value="Add to cart">' +
+                        '</form>' +
+                        '</td>';
                 returnString += "</tr>";
-            }
-
+            });
             returnString += "</tbody>";
         }
 
         return returnString;
     }
+    
 }
