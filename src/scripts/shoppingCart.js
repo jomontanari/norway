1c2e85bf6e463490cf0490b4fbb7da583372dfa6
Luca Grulla <luca.grulla@gmail.com>
2010-05-25 23:22:50 +0100

    Luca: refactored towarda browser.Dom

--- src/scripts/shoppingCart.js
+++ src/scripts/shoppingCart.js
@@ -1,4 +1,4 @@
-function ShoppingCart() {
+function ShoppingCart(shoppingCartView) {
 
     var items = new Array();
 
@@ -9,17 +9,11 @@ function ShoppingCart() {
 
 
     function modifyShoppingCartViewWithAnAdditional(item) {
-        $(".itemList ul").append("<li>");
-        $(".itemList ul li:last")
-                .append(item.description)
-                .append(" - ")
-                .append(item.price);
-        $(".itemList ul").append("</li>");
+        shoppingCartView.modifyShoppingCartViewWithAnAdditional(item);
+        shoppingCartView.updateOrderPrice(orderPrice());
+    };
 
-        $(".totalPrice").text("Total price: " + orderPrice());
-    }
-
-    orderPrice = function() {
+    function orderPrice() {
         var price = 0;
         $.each(items, function(index, value) {
             price = price + value.price;
@@ -27,43 +21,16 @@ function ShoppingCart() {
         return price;
     };
 }
-
 
 function ShoppingCartView() {
 
     this.modifyShoppingCartViewWithAnAdditional = function(item) {
         Browser.Dom.append(".itemList ul", "<li>");
         Browser.Dom.append(".itemList ul li:last",item.description + " - " + item.price);
-        Browser.Dom.append(".totalPrice", "Total price: " + orderPrice());
+        Browser.Dom.append(".itemList ul", "</li>");
     };
 
     this.updateOrderPrice = function(price) {
         Browser.Dom.text(".totalPrice", "Total price: " + price);
     }
-}
-
-
-function ShoppingCart(shoppingCartView) {
-
-    var view = shoppingCartView;
-    var items = new Array();
-
-    this.add = function(item) {
-        items.push(item);
-        modifyShoppingCartViewWithAnAdditional(item);
-    };
-
-
-    function modifyShoppingCartViewWithAnAdditional(item) {
-        view.modifyShoppingCartViewWithAnAdditional(item);
-        view.updateOrderPrice(orderPrice());
-    }
-
-    this.orderPrice = function() {
-        var price = 0;
-        $.each(items, function(index, value) {
-            price = price + value.price;
-        });
-        return price;
-    };
 }
