65f83e3ac40de5a82e29935f3cb97f5af8abee2a
Brian Blignaut <gurrie09@hotmail.com>
2010-06-04 09:38:05 +0100

    Changed dom tests to use HtmlDoc

--- tests/unit/domTests.js
+++ tests/unit/domTests.js
@@ -1,47 +1,33 @@
 TestCase("DomTests", {
-    setUp : function() {
-        mockControl = new MockControl();
-
-        jQueryMock = mockControl.createDynamicMock($.fn);
-    },      
-
-    tearDown : function() { 
-        mockControl.verify();
-    },
-    
     test_should_register_the_passed_function_as_a_listener_to_the_click_event_of_the_element : function() {
-        var selector = ".class-name";
+        /*:DOC += <div><input type="button" class="class-name"/></div>*/
+        var clicked = false;
 
-        jQueryMock.expects().init(selector, null).toReturn(jQueryMock);
-        jQueryMock.expects().click(Arg.isA(Function));
+        Browser.Dom.addClickHandler(".class-name", function() { clicked = true; });
+        $(".class-name").click();
 
-        Browser.Dom.addClickHandler(selector, function() {});
+        assertTrue(clicked);
     },
 
     test_should_set_the_passed_html_on_the_element : function() {
-        var selector = ".class-name";
+        /*:DOC += <div class="class-name"></div>*/
 
-        jQueryMock.expects().init(selector, null).toReturn(jQueryMock);
-        jQueryMock.expects().html("html");
+        Browser.Dom.setHtml(".class-name", "html");
 
-        Browser.Dom.setHtml(selector, "html");
+        assertEquals("html", $(".class-name").html());
     },
 
     test_should_return_the_value_of_the_element : function() {
-        var selector = ".class-name";
+        /*:DOC += <div><input type="text" class="class-name" value="1"/></div>*/
 
-        jQueryMock.expects().init(selector, null).toReturn(jQueryMock);
-        jQueryMock.expects().val().toReturn("1");
-
-        assertEquals("1", Browser.Dom.getValue(selector));
+        assertEquals("1", Browser.Dom.getValue(".class-name"));
     },
 
     test_should_enable_the_passed_element : function() {
-        var selector = ".class-name";
+        /*:DOC += <div><input type="text" class="class-name" disabled="disabled"/></div>*/
 
-        jQueryMock.expects().init(selector, null).toReturn(jQueryMock);
-        jQueryMock.expects().removeAttr("disabled");
+        Browser.Dom.enable(".class-name");
 
-        Browser.Dom.enable(selector);
+        assertFalse("", $(".class-name").attr("disabled"));
     }
 });
